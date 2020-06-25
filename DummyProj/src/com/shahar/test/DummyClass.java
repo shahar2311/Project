c9e051f247a61d43f583022573de66b4370dfe08
shahar2311 <67388888+shahar2311@users.noreply.github.com>
2020-06-25 21:13:42 +0300

    Update DummyClass.java
    double row

--- DummyProj/src/com/shahar/test/DummyClass.java
+++ DummyProj/src/com/shahar/test/DummyClass.java
@@ -4,6 +4,7 @@ public class DummyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello World welcome");
+		System.out.println("Hello World welcome");
 
 	}
 
